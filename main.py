72a78c988487c8ee7c05d77f7bf2eb5ccf9e0e23
Andrew Tran <andtran0703@gmail.com>
2021-11-13 20:38:29 -0600

    tentative final script ver

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import search
-import os
-import json
+import time, os, json, search, score
 
-def __main__():
+starttime = time.time()
+
+def __main__(searchTerm):
     # clear output files
     if os.path.exists("./outputFiles/tweets.json"):
         os.remove("./outputFiles/tweets.json")
@@ -10,7 +10,7 @@ def __main__():
         os.remove("./outputFiles/usernames.json")
     
     # run search by keyword
-    search.searchTweet("AAPL")
+    search.searchTweet(str(searchTerm))
     
     # map tweets to author
     tweets = []
@@ -23,5 +23,18 @@ def __main__():
         search.searchUser(str(i["username"]))
 
     print("Finished adding users")
-    
-__main__()
+
+    weight = score.calcScore()
+
+
+    resultDict = {
+        "weight": weight,
+        "tweets": tweets
+    }
+    with open("./outputFiles/result.json", "w", encoding="utf-8") as f:
+        json.dump(resultDict, f, ensure_ascii=False, indent = 4)
+        
+
+__main__("$AAPL") # TODO incorporate serach term
+
+print("--- %s seconds ---" % (time.time() - starttime))
